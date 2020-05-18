c67ad7c5f4bee118d8d585660ea207964bd5284a
meeyoungchoi <goodlife1359@gmail.com>
2020-05-18 13:38:57 +0900

    Initial commit

---
+++ src/main/java/com/example/myblog/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.myblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "helloworld";
+    }
+
+    @GetMapping("/greetings")
+    public String greetings() {
+        return "greetings";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+}
